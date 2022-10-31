bbca80d5db196099622cc68f8b7ff6f5823bd1fd
ionutauc18 <108615889+ionutauc18@users.noreply.github.com>
2022-10-31 16:10:43 +0000

    Delete Basic_JavaScript_1.js

--- Basic_JavaScript_1.js
+++
@@ -1,7 +0,0 @@
-alert("Hello, World! I am an alert box!");
-       function My_first_Function()  {
-                var String = "Kiss me, I'm Irish!";
-                var result = String.fontcolor("green");
-                document.getElementById("Irish").innerHTML=
-                result;
-       }
